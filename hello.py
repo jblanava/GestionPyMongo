05bff9420da9f524791f21f1d92e419562accd49
jblanava <0619836326@uma.es>
2020-11-28 12:27:03 +0100

    dir mongodb

--- hello.py
+++ hello.py
@@ -7,3 +7,5 @@ print("laMaria")
 print("elBrayano")
 print("prueba")
 print("adri intenta subirlo bien")
+
+# mongodb+srv://Gestionpymongo:<password>@cluster0.iixvr.mongodb.net/<dbname>?retryWrites=true&w=majority
