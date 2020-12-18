22740ea3e82c3ca93906e27e6e8a6d59e22a0ca0
YerayRuiz <yeray.r7@uma.es>
2020-12-18 13:06:59 +0100

    todo funciona correctly

--- People.py
+++ People.py
@@ -1,7 +1,28 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+import tkinter.font as tkFont
 import pymongo
 
 
 class People:
+
+    @staticmethod
+    def tuplas_personas():
+        client = pymongo.MongoClient("mongodb+srv://Gestionpymongo:Gestionpymongo@cluster0.iixvr.mongodb.net"
+                                     "/Gestiondb?retryWrites=true&w=majority")
+        mydb = client["Gestiondb"]
+        mycol = mydb["People"]
+        mydoc = mycol.find()
+        lista = []
+
+        for x in mydoc:
+            id_person = x.get("_id")
+            firstname_person = x.get("firstname")
+            lastname_person = x.get("lastname")
+            tupla = (id_person, firstname_person, lastname_person)
+            lista.append(tupla)
+
+        return lista
 
     @staticmethod
     def lista_amigos(_id):
@@ -127,6 +148,183 @@ class Friends:
         self._id2 = None
 
 
+#GUI
+
+#PRECAUCION -> PONER EL ORDEN DE LOS COMPONENTES SEGUN SE VAYAN A MOSTRAR, ejecutar este script y cambiar de lugar el
+# codigo del label, vereis que pasa
+
+
+class Window(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.master = master
+
+
+#CREAR TABLA
+#Prueba de tabla
+class Table(tk.Frame):
+    def __init__(self, parent=None, title="", headers=[], height=10, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self._title = tk.Label(self, text=title, font=("Helvetica", 20), bg="#51d1f6")
+        self._headers = headers
+        self._tree = ttk.Treeview(self,
+                                  height=height,
+                                  columns=self._headers,
+                                  show="headings", selectmode=tk.BROWSE)
+        self._title.pack(side=tk.TOP, fill="x")
+
+        # Agregamos dos scrollbars
+        vsb = ttk.Scrollbar(self, orient="vertical", command=self._tree.yview)
+        vsb.pack(side='right', fill='y')
+        #hsb = ttk.Scrollbar(self, orient="horizontal", command=self._tree.xview)
+        #hsb.pack(side='bottom', fill='x')
+
+        #xscrollcommand=hsb.set,
+        self._tree.configure(yscrollcommand=vsb.set)
+        self._tree.pack(side="left")
+        #MANEJADOR DE TREE
+        self._tree.tag_bind("mytag", "<<TreeviewSelect>>", self.item_selected)
+
+        for header in self._headers:
+            self._tree.heading(header, text=header.title())
+            self._tree.column(header, stretch=True,
+                              width=tkFont.Font().measure(header.title()))
+
+    def add_row(self, row):
+        self._tree.insert('', 'end', values=row, tags=("mytag",))
+        for i, item in enumerate(row):
+            col_width = tkFont.Font().measure(item)
+            if self._tree.column(self._headers[i], width=None) < col_width:
+                    self._tree.column(self._headers[i], width=col_width)
+
+    def item_selected(self, event):
+        lAmigos.delete('0','end')
+        lDisponibles.delete('0','end')
+        item = self._tree.selection()[0]
+        tupla = self._tree.item(item, option="values")
+        index = int(tupla[0])
+        listaAmigos = People.lista_amigos(index)
+        listaDisponibles = People.lista_disponibles(index)
+
+        cont = 1
+        for x in listaAmigos:
+            lAmigos.insert(cont, x)
+            cont = cont+1
+        cont = 1
+        for y in listaDisponibles:
+            lDisponibles.insert(cont, y)
+            cont = cont+1
+
+def insertFriend():
+    try:
+        #id de la tabla seleccionado
+        table_selected = personas_tab._tree.selection()[0]
+        table_tupla = personas_tab._tree.item(table_selected, option="values")
+        table_index = int(table_tupla[0])
+
+        #id de la lista disponibles
+        dis_selected = lDisponibles.selection_get()
+        dis_id = int(dis_selected.split(";")[0])
+
+        #insertar en la bd si se han seleccionado
+        if table_index is not None and dis_id is not None:
+            Friends(table_index, dis_id)
+    except:
+        messagebox.showwarning(message="No se ha seleccionado un usuario", title="Error")
+
+def deleteFriend():
+    # id de la tabla seleccionado
+    try:
+        table_selected = personas_tab._tree.selection()[0]
+        table_tupla = personas_tab._tree.item(table_selected, option="values")
+        table_index = int(table_tupla[0])
+
+        # id de la lista disponibles
+        amigos_selected = lAmigos.selection_get()
+        amigos_id = int(amigos_selected.split(";")[0])
+
+        #Si los seleccionados no son vacios se borra
+        if table_index is not None and amigos_id is not None:
+            f = Friends(table_index, amigos_id)
+            f.delete()
+    except:
+        messagebox.showwarning(message="No se ha seleccionado un usuario", title="Error")
+
+
+root = tk.Tk()
+root.config(bg="#51d1f6")
+Window(root)
+
+# set window title
+root.wm_title("Rastreator") #pone el titulo a la ventana
+root.geometry('1200x700') #tamaño de la ventana
+#lbl = tk.Label(root, text="Label") #crea un label e indica en que ventana esta el label -> root
+#lbl.grid(column=0, row=0) #especifica la posicion
+#lbl.pack()
+#B = tk.Button(root, text ="Hello", font=("Arial Bold", 20)) #crea un boton y pone un texto y un tamaño
+#B.pack() #empaqueta al boton
+#listbox = tk.Listbox() #crea un listbox donde mostrar los datos
+#listbox.insert(0, "Python") #en la pos 0 pon al elemento...
+#listbox.insert(1, "use")
+#listbox.insert(2, "c++")
+#listbox.insert(tk.END, "Java") #en la ultima posicion pon el elemento...
+#listbox.pack() #empaquetar la lista
+
+personas_headers = (u"ID", u"FIRSTNAME", u"LASTNAME")
+
+personas_tab = Table(root, title="Rastreator", headers=personas_headers)
+personas_tab.place(x=500, y=0)
+
+cursor = People.tuplas_personas()
+
+for row in cursor:
+    personas_tab.add_row(row)
+
+#LISTA DE AMIGOS
+#label
+labelAmigos = tk.Label(root, text="Amigos", font=("Trebuchet MS", 14), bg="#51d1f6")
+labelAmigos.place(x=300, y=320)
+
+#listbox
+lAmigos = tk.Listbox(font=("Trebuchet MS", 12)) #crea un listbox donde mostrar los datos
+lAmigos.config(width=40, height=12)
+lAmigos.place(x=175, y=350)
+
+
+#BOTONES
+#boton de insertar
+canvas = tk.Canvas(root, width=50, height=50)
+
+imagetest = tk.PhotoImage(file="flecha_verde.png")
+canvas.create_image(50, 50, image=imagetest)
+
+bIns = tk.Button(root, image=imagetest, command=insertFriend) #crea un boton y pone un texto y un tamaño
+bIns.config(width=50, height=50)
+bIns.place(x = 575, y = 400)
+
+#boton de eliminar
+canvas2 = tk.Canvas(root, width=50, height=50)
+
+imagetest2 = tk.PhotoImage(file="flecha_roja.png")
+canvas2.create_image(50, 50, image=imagetest2)
+
+bDel = tk.Button(root, image=imagetest2, command=deleteFriend) #crea un boton y pone un texto y un tamaño
+bDel.config(width=50, height=50)
+bDel.place(x = 575, y = 500)
+
+#LISTA DE DISPONIBLES
+#label
+labelDis = tk.Label(root, text="Disponibles", font=("Trebuchet MS", 14), bg="#51d1f6")
+labelDis.place(x=825, y=320)
+
+#listbox
+lDisponibles = tk.Listbox(font=("Trebuchet MS", 12))
+lDisponibles.config(width=40, height=12)
+lDisponibles.place(x=700, y=350)
+
+root.mainloop() # show window
+
+'''
 amigos1 = People.lista_amigos(1)
 print("Panas: ")
 for x in amigos1:
@@ -136,5 +334,4 @@ print("Now disp: ")
 disp1 = People.lista_disponibles(1)
 for x in disp1:
     print(x)
-
-
+'''
